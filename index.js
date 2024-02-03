f3c5a4fd401053f8327f07dc0ab03417c42e7544
dev-run-sp <ajattmohamedo@gmail.com>
2024-02-03 17:08:37 -0600

    adding db changes

--- index.js
+++ index.js
@@ -27,7 +27,14 @@ app.post('/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
+  let priorItems = await db.collection(col).get(key)
+  if (priorItems == null) {
+    priorItems = [priorItems]
+    priorItems.concat(req.body)
+  } else {
+    priorItems.concat(req.body)
+  }
+  const item = await db.collection(col).set(key, priorItems)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })
